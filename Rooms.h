fb1e17acb91d82e2e7f4d335806c780b4e4dcd04
Fijona999 <80392331+Fijona999@users.noreply.github.com>
2021-03-10 13:14:19 +0200

    Rooms.h

---
+++ Rooms.h
@@ -0,0 +1,20 @@
+#ifndef ROOMS_H
+#define ROOMS_H
+#include <stdexcept>
+
+using namespace {
+           class Rooms {
+               public:
+                     string colour;
+                     vector<string> rooms;
+                     Rooms()
+                    ~Rooms()
+                     void setColour(string input)
+                    string getColour() 
+                    void addRooms(string input)
+                    void printRooms()
+  
+	  };
+  }
+	
+#endif
