ee6b6d4d4f6174ce16ed7e6f2eecd273aef87b8c
Fijona999 <80392331+Fijona999@users.noreply.github.com>
2021-03-10 12:54:45 +0200

    Update Rooms.cpp

--- Rooms.cpp
+++ Rooms.cpp
@@ -35,28 +35,3 @@ public:
     }
 
 };
-int main()
-{
-    vector<Rooms>names;
-
-    Rooms r1;
-    r1.setColour("Red:");
-    r1.addRooms("Kitchen");
-    r1.addRooms("Bedroom");
-    r1.addRooms("Garage");
-    r1.getColour();
-    r1.printRooms();
-
-    Rooms r2;
-    r2.setColour("Blue:");
-    r2.addRooms("Nursery");
-    r2.addRooms("Games room");
-    r2.addRooms("Shower");
-    names.push_back(r2);
-
-    for(int i=0; i<names.size(); ++i)
-    {
-        names[i].printRooms();
-    }
-    return 0;
-}
